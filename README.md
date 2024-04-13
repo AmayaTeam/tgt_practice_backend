b962435d1eeb17474a448a928700e357bf6ac91a
Stepan <stepantishhen@mail.ru>
2024-04-13 20:08:50 +0300

    Fix README.md and adding migration file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # TGT Весенняя технологическая практика
+
+## Чтобы запустить проект
+1. Запуск проекта
+   * Собрать и запустить проект
+     ```commandline
+     docker-compose up -d --build
+     ```
+   * Запустить проект
+     ```commandline
+     docker-compose up -d
+     ```
+   * Остановить проект
+     ```commandline
+     docker-compose stop
+     ```
--- api/models.py
+++ api/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.db import models
 
 
-# TODO: TextField?
 class ToolModuleGroup(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(null=True, blank=True)
@@ -11,27 +10,27 @@ class ToolModuleGroup(models.Model):
 
 class ToolModuleType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    r_modules_group_id = models.ForeignKey(ToolModuleGroup, on_delete=models.CASCADE)  # TODO: module(s)?
+    r_modules_group_id = models.ForeignKey(ToolModuleGroup, on_delete=models.CASCADE)
     name = models.TextField(null=True, blank=True)
-    # TODO: module_type_id?
-    hash_code = models.TextField(null=True, blank=True)  # TODO: для чего, зачем
+    module_type_id = models.TextField(null=True, blank=True)
+    hash_code = models.TextField(null=True, blank=True)
 
 
 class ToolModule(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     r_module_type_id = models.ForeignKey(ToolModuleType, on_delete=models.CASCADE)
-    sn_ = models.TextField(max_length=255, null=True, blank=True)
-    dbdate_ = models.DateField(null=True, blank=True)
-    dbversion_ = models.DateField(null=True, blank=True)
-    dbsn_ = models.TextField(null=True, blank=True)
-    dbcomment_ = models.TextField(null=True, blank=True)
-    dbtname_ = models.TextField(null=True, blank=True)
-    dbtlength_ = models.FloatField(null=True, blank=True)
-    dbtweight_ = models.FloatField(null=True, blank=True)
-    dbtmax_od_ = models.FloatField(null=True, blank=True)
-    dbtmax_od_collapsed_ = models.FloatField(null=True, blank=True)
-    dbtmax_od_opened_ = models.FloatField(null=True, blank=True)
-    dbtimage2d_ = models.FloatField(null=True, blank=True)
+    sn = models.TextField(max_length=255, null=True, blank=True)
+    dbdate = models.DateField(null=True, blank=True)
+    dbversion = models.DateField(null=True, blank=True)
+    dbsn = models.TextField(null=True, blank=True)
+    dbcomment = models.TextField(null=True, blank=True)
+    dbtname = models.TextField(null=True, blank=True)
+    dbtlength = models.FloatField(null=True, blank=True)
+    dbtweight = models.FloatField(null=True, blank=True)
+    dbtmax_od = models.FloatField(null=True, blank=True)
+    dbtmax_od_collapsed = models.FloatField(null=True, blank=True)
+    dbtmax_od_opened = models.FloatField(null=True, blank=True)
+    dbtimage2d = models.FloatField(null=True, blank=True)
     dbtimage_h_shift = models.FloatField(null=True, blank=True)
     dbtimage_h_scale = models.FloatField(null=True, blank=True)
     dbtimage_h_y1 = models.FloatField(null=True, blank=True)
@@ -42,11 +41,10 @@ class ToolModule(models.Model):
 class ToolSensorType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.TextField(null=True, blank=True)
-    # TODO: sensor_id?
 
 
 class ToolInstalledSensor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     r_toolmodule_id = models.ForeignKey(ToolModule, on_delete=models.CASCADE)
     r_toolsensortype_id = models.ForeignKey(ToolSensorType, on_delete=models.CASCADE)
-    record_point_ = models.FloatField(null=True, blank=True)
+    record_point = models.FloatField(null=True, blank=True)
