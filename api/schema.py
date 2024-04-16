6d63790c274fa9cc8c5961891912657bff308843
elenagernichenko <elena.gernichenko@yandex.ru>
2024-04-17 01:45:36 +0300

    added Queries for ToolModule, ToolSensorType, ToolInstalledSensor models (but not tested)

--- api/schema.py
+++ api/schema.py
@@ -9,10 +9,9 @@ from .models import *
 class Query(graphene.ObjectType):
     tool_module_groups = graphene.List(ToolModuleGroupT)
     tool_module_types = graphene.List(ToolModuleTypeT)
-
-    # tool_modules = graphene.List(ToolModuleT)
-    # tool_sensor_types = graphene.List(ToolSensorTypeT)
-    # tool_installed_sensors = graphene.List(ToolInstalledSensorT)
+    tool_modules = graphene.List(ToolModuleT)
+    tool_sensor_types = graphene.List(ToolSensorTypeT)
+    tool_installed_sensors = graphene.List(ToolInstalledSensorT)
 
     def resolve_tool_module_groups(self, info, **kwargs):
         return ToolModuleGroup.objects.all()
@@ -20,14 +19,14 @@ class Query(graphene.ObjectType):
     def resolve_tool_module_types(self, info, **kwargs):
         return ToolModuleType.objects.all()
 
-    # def resolve_tool_modules(self, info, **kwargs):
-    #     return ToolModule.objects.all()
-    #
-    # def resolve_tool_sensor_types(self, info, **kwargs):
-    #     return ToolSensorType.objects.all()
-    #
-    # def resolve_tool_installed_sensors(self, info, **kwargs):
-    #     return ToolInstalledSensor.objects.all()
+    def resolve_tool_modules(self, info, **kwargs):
+        return ToolModule.objects.all()
+
+    def resolve_tool_sensor_types(self, info, **kwargs):
+        return ToolSensorType.objects.all()
+
+    def resolve_tool_installed_sensors(self, info, **kwargs):
+        return ToolInstalledSensor.objects.all()
 
 
 @convert_django_field.register(models.BinaryField)
--- api/types.py
+++ api/types.py
@@ -7,7 +7,6 @@ class ToolModuleGroupT(DjangoObjectType):
     class Meta:
         model = ToolModuleGroup
 
-
 class ToolModuleGroupConnection(graphene.relay.Connection):
     class Meta:
         node = ToolModuleGroupT
@@ -17,7 +16,33 @@ class ToolModuleTypeT(DjangoObjectType):
     class Meta:
         model = ToolModuleType
 
-
 class ToolModuleTypeConnection(graphene.relay.Connection):
     class Meta:
         node = ToolModuleTypeT
+
+
+class ToolModuleT(DjangoObjectType):
+    class Meta:
+        model = ToolModule
+
+class ToolModuleConnection(graphene.relay.Connection):
+    class Meta:
+        node = ToolModuleT
+
+
+class ToolSensorTypeT(DjangoObjectType):
+    class Meta:
+        model = ToolSensorType
+
+class ToolSensorTypeConnection(graphene.relay.Connection):
+    class Meta:
+        node = ToolSensorTypeT
+
+
+class ToolInstalledSensorT(DjangoObjectType):
+    class Meta:
+        model = ToolInstalledSensor
+
+class ToolInstalledSensorConnection(graphene.relay.Connection):
+    class Meta:
+        node = ToolInstalledSensor
