1540d1840ab54bb12d4fc47a053525981b9b889b
Stepan <stepantishhen@mail.ru>
2024-04-21 00:03:06 +0300

    Fix CORS for send data to frontend

--- tools_app/settings.py
+++ tools_app/settings.py
@@ -113,7 +113,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://127.0.0.1:8000",
     "http://localhost:8000",
+    "http://localhost:3000",
     "https://localhost:8000",
+    "https://localhost:3000",
 ]
 
 CORS_ALLOWED_METHODS = [
